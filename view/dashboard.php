fcbcb9ba39d1989078085e0b8129c96c84b2a786
stela montenegro <stelamontenegro21@gmail.com>
2024-09-17 21:33:44 -0300

    Update dashboard.php

--- view/dashboard.php
+++ view/dashboard.php
@@ -357,19 +357,19 @@
     new Chart(ctx_pie, {
       type: 'doughnut',
             data: {
-                labels: ['Pix', 'Crédito', 'Boleto'],
+                labels: ['Débito', 'Crédito', 'Pix'],
                 datasets: [{
                     label: '# of Votes',
                     data: [12, 19, 20],
                     backgroundColor: [
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 199, 60, 0.2)'
+                        'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)'
                     ],
                     borderColor: [
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 199, 60, 0.2)'
+                       'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)'
                     ],
                     borderWidth: 2
                 }]
